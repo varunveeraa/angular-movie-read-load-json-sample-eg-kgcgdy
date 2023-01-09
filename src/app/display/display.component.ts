02730ffa38db5c3ff8391dcd9995ef58b1b22096
varunveeraa <54136118+varunveeraa@users.noreply.github.com>
2023-01-09 18:01:49 +0530

    3 files modified

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -4,23 +4,24 @@ import { ContactService } from '../contacts.service';
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
-  styleUrls: ['./display.component.css']
+  styleUrls: ['./display.component.css'],
 })
 export class DisplayComponent implements OnInit {
   public contacts: any;
 
   CustId: any;
-  constructor(private myService: ContactService) { }
+  constructor(private myService: ContactService) {}
 
   public async ngOnInit(): Promise<void> {
-    this.myService.onCustomerSelect.subscribe(value => {
+    this.myService.onCustomerSelect.subscribe((value) => {
       this.CustId = value;
-    })
+    });
   }
 
-  updateBalance(val, index){
-    console.log(val, index)
+  changeBal(i) {
+    this.CustId.invoices[i].balance -= this.CustId.invoices[i].amount_received;
+    if (this.CustId.invoices[i].balance == 0) {
+      this.CustId.invoices[i].payment_status === 'complete';
+    }
   }
-
-
 }
