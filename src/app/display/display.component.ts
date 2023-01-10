dc37ba2c17d9b2e007c9044f9730f4922b19e0a7
varunveeraa <54136118+varunveeraa@users.noreply.github.com>
2023-01-10 14:58:17 +0530

    3 files modified

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -9,6 +9,8 @@ import { ContactService } from '../contacts.service';
 export class DisplayComponent implements OnInit {
   public contacts: any;
 
+  amountReceived: number = 0;
+
   CustId: any;
   constructor(private myService: ContactService) {}
 
@@ -20,8 +22,29 @@ export class DisplayComponent implements OnInit {
 
   changeBal(i) {
     this.CustId.invoices[i].balance -= this.CustId.invoices[i].amount_received;
-    if (this.CustId.invoices[i].balance == 0) {
-      this.CustId.invoices[i].payment_status === 'complete';
+    if (this.CustId.invoices[i].balance === 0) {
+      this.CustId.invoices[i].payment_status == 'complete';
+    }
+  }
+
+  findSum(i) {
+    let sum = 0;
+    if (i == true) {
+      for (let x = 0; x < this.CustId.invoices.length; x++) {
+        sum += this.CustId.invoices[x].balance;
+      }
+      return sum;
+    } else {
+      for (let x = 0; x < this.CustId.invoices.length; x++) {
+        sum += this.CustId.invoices[x].amount_received;
+      }
+      return sum;
+    }
+  }
+
+  checkAmt() {
+    if (this.amountReceived > this.CustId.amount_receivable) {
+      this.CustId.credits+=(this.amountReceived - this.CustId.amount_receivable)
     }
   }
 }
