883766621aaa951989f406e72addd19466f91a4a
varunveeraa <54136118+varunveeraa@users.noreply.github.com>
2023-01-12 15:58:21 +0530

    3 files modified

--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactService } from '../contacts.service';
+import * as _ from 'lodash';
+import * as cloneDeep from 'lodash';
 
 @Component({
   selector: 'app-display',
@@ -23,77 +25,100 @@ export class DisplayComponent implements OnInit {
 
   credVal: number = 0;
 
+  showContent: any;
+
   constructor(private myService: ContactService) {}
+
+  ///////////////////////////////////////////////////////////////////////
 
   public async ngOnInit(): Promise<void> {
     this.myService.onCustomerSelect.subscribe((value) => {
       this.CustId = value;
+      // this.CustIdc = { ...this.CustId };
+      // this.CustIdc = cloneDeep(this.CustId);
+      this.CustIdc = JSON.parse(JSON.stringify(this.CustId));
     });
-    this.CustIdc = { ...this.CustId };
   }
+
+  ///////////////////////////////////////////////////////////////////////
 
   lilnigga() {
-    let nigga = false;
-
-    if (nigga == false) {
-      alert(this.CustIdc);
-    }
+    console.log(_.isEqual(this.CustIdc, this.CustId));
   }
+
+  ///////////////////////////////////////////////////////////////////////
 
   toggleDisplayDivIf() {
     this.isShowDivIf = !this.isShowDivIf;
   }
 
+  ///////////////////////////////////////////////////////////////////////
+
   updateCredApply() {
     this.totalRcvdAmt = this.credVal + this.amountReceived;
   }
 
+  ///////////////////////////////////////////////////////////////////////
+
   changeBal(i) {
     if (
-      this.CustId.invoices[i].amount_received >
-      this.CustId.invoices[i].actual_due
+      this.CustIdc.invoices[i].amount_received >
+      this.CustIdc.invoices[i].actual_due
     ) {
       alert('enter valid amount');
-      this.CustId.invoices[i].amount_received = 0;
+      this.CustIdc.invoices[i].amount_received = 0;
     } else if (this.amountReceived === 0) {
       alert('enter amount received');
-      this.CustId.invoices[i].amount_received = 0;
-    } else if (this.findSum(false) > this.CustId.invoices[i].amount_received) {
-      alert('insufficent funds');
-      this.CustId.invoices[i].amount_received = 0;
-    } else {
-      this.CustId.invoices[i].balance -=
-        this.CustId.invoices[i].amount_received;
-      if (this.CustId.invoices[i].balance === 0) {
-        this.CustId.invoices[i].payment_status = 'complete';
+      this.CustIdc.invoices[i].amount_received = 0;
+    }
+    // else if (this.findSum(false) > this.CustIdc.invoices[i].amount_received) {
+    //   alert('insufficent funds');
+    //   this.CustIdc.invoices[i].amount_received = 0;
+    // }
+    else {
+      this.CustIdc.invoices[i].balance -=
+        this.CustIdc.invoices[i].amount_received;
+      if (this.CustIdc.invoices[i].balance === 0) {
+        this.CustIdc.invoices[i].payment_status = 'complete';
       } else if (
-        this.CustId.invoices[i].balance > 0 &&
-        this.CustId.invoices[i].balance != 0
+        this.CustIdc.invoices[i].balance > 0 &&
+        this.CustIdc.invoices[i].balance != 0
       ) {
-        this.CustId.invoices[i].payment_status = 'partially complete';
+        this.CustIdc.invoices[i].payment_status = 'partially complete';
       }
     }
   }
+
+  ///////////////////////////////////////////////////////////////////////
 
   findSum(i) {
     let sum = 0;
     if (i == true) {
-      for (let x = 0; x < this.CustId.invoices.length; x++) {
-        sum += this.CustId.invoices[x].balance;
+      for (let x = 0; x < this.CustIdc.invoices.length; x++) {
+        sum += this.CustIdc.invoices[x].balance;
       }
       return sum;
     } else {
-      for (let x = 0; x < this.CustId.invoices.length; x++) {
-        sum += this.CustId.invoices[x].amount_received;
+      for (let x = 0; x < this.CustIdc.invoices.length; x++) {
+        sum += this.CustIdc.invoices[x].amount_received;
       }
       return sum;
     }
   }
 
+  ///////////////////////////////////////////////////////////////////////
+
   checkAmt() {
-    if (this.amountReceived > this.CustId.amount_receivable) {
-      this.CustId.credits +=
-        this.amountReceived - this.CustId.amount_receivable;
+    if (this.amountReceived > this.CustIdc.amount_receivable) {
+      this.CustIdc.credits +=
+        this.amountReceived - this.CustIdc.amount_receivable;
     }
+  }
+
+  ///////////////////////////////////////////////////////////////////////
+
+  saveData() {
+    this.CustId = { ...this.CustIdc };
+    console.log(this.CustId);
   }
 }
