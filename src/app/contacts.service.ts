51d377358a232173bb5c6716f567ad22564ca0e4
varunveeraa <54136118+varunveeraa@users.noreply.github.com>
2023-01-31 14:47:12 +0530

    1 file modified

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -11,17 +11,7 @@ export class ContactService {
   constructor(private http: HttpClient) {}
 
   public getCustomers(id: string): Promise<ICustomer> {
-    const apiUrl: string = 'http://localhost:6969/Customers/';
-
-    // var xhr = new XMLHttpRequest();
-    // xhr.open('GET', 'http://localhost:6969/Customers/', true);
-    // xhr.onreadystatechange = function () {
-    //   if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-    //     var data = JSON.parse(xhr.responseText);
-    //     console.log(data);
-    //   }
-    // };
-    // xhr.send();
+    const apiUrl: string = '../../assets/customers.json';
 
     return this.http.get<ICustomer>(apiUrl + id).toPromise();
   }
